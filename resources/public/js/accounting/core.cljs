7ba83a4ae5557bcd64186634de365ad707111200
Ulrich Schinz <info@schinz.it>
2016-02-26 13:19:06 +0100

    playin around

--- resources/public/js/accounting/core.cljs
+++ resources/public/js/accounting/core.cljs
@@ -3,5 +3,23 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
-(enable-console-print!)
-(println "hello world!")
+(def app-state (atom {:count 0}))
+
+(defui Counter
+  Object
+  (render [this]
+          (let [{:keys [count]} (om/props this)]
+            (dom/div nil
+              (dom/span nil (str "Count: " count))
+              (dom/br nil nil)
+              (dom/button
+                #js {:onClick
+                     (fn [e]
+                       (swap! app-state update-in [:count] inc))}
+                "Click me!")))))
+
+(def reconciler
+  (om/reconciler {:state app-state}))
+
+(om/add-root! reconciler
+  Counter (gdom/getElement "app"))
